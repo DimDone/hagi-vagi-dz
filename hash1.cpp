ed9286ea29aabae2c85afc4ca31a7caab689550a
DimDone <kitaev-mitynka@mail.ru>
2022-05-29 16:34:47 +0300

    created template of closed hashing

--- hash1.cpp
+++ hash1.cpp
@@ -3,21 +3,23 @@
 #include <vector>
 #include <string>
 #include <list>
+#include <cmath>
 
 using namespace std;
 
 ifstream in("workers.txt");
 
-const int m = 10;
+const int m = 16;
 
-int hashFuction(int x){
-    return x%m;
+int hashFuction(float x){
+    x *= 0.618034;
+    float z;
+    return modf(x, &z) * m;
 }
 
 int findYear(string s){
     return stoi(s.substr(s.length() - 4));
 }
-
 
 vector <list<string>> createTeble(){
     string s;
@@ -25,7 +27,12 @@ vector <list<string>> createTeble(){
     while (in.peek() != EOF){
         getline(in, s);
         int k = hashFuction(findYear(s));
-        n[k].push_back(s);
+        if (n[k].size() == 0)
+            n[k].push_back(s);
+        else{
+            k++;
+            n[k].push_back(s);
+        }
     }
     return n;
 }
@@ -34,7 +41,7 @@ void outputTable(vector <list <string>> n){
     for(vector <list<string>>:: iterator it = n.begin(); it != n.end(); it++){
          list<string> a = *it;
          for(list<string>::iterator iter = a.begin(); iter != a.end(); iter++)
-            cout << *iter << " | ";
+            cout << *iter;
          if (a.size() == 0)
             cout << "NULL" << endl;
          else cout << endl;
