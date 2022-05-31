0771de359baffa3274e2a6b5555c292b659a96f8
DimDone <kitaev-mitynka@mail.ru>
2022-05-31 15:56:29 +0300

    remove the edge of two points

--- grtemplate.cpp
+++ grtemplate.cpp
@@ -7,55 +7,56 @@ using namespace std;
 
 ifstream in("data.txt");
 
-map<int, list<int>> MakeGr(bool orient){ //создание списка смежности графа
-    map<int, list<int>> gr;
+map<int, list<int>> MakeGr(bool orient) { //создание списка смежности графа
+    map<int, list<int>> l_Adj;
     int x, y; // x<->y or x->y
     while (in.peek() != EOF) {
         in >> x >> y;
-        auto it_x = gr.find(x);
+        auto it_x = l_Adj.find(x);
         bool fl = true;
-        if (it_x != gr.end())
-        {
+        if (it_x != l_Adj.end()) {
             list<int> a = it_x->second;
             for (auto iter = a.begin(); iter != a.end(); iter++)
-                if (*iter == y)
-                {
+                if (*iter == y) {
                     fl = false;
                     break;
                 }
         }
-        if (orient)
-        {
-            if (fl)
-            {
-                gr[x].push_back(y);
-                if (gr.find(y) == gr.end())
-                {
-                    gr[y].push_back(0);
-                    auto it = gr.find(y);
+        if (orient) {
+            if (fl) {
+                l_Adj[x].push_back(y);
+                if (l_Adj.find(y) == l_Adj.end()) {
+                    l_Adj[y].push_back(0);
+                    auto it = l_Adj.find(y);
                     it->second.erase(it->second.begin());
                 }
             }
         }
-        else if (fl)
-        {
-            gr[x].push_back(y);
-            gr[y].push_back(x);
+        else if (fl) {
+            l_Adj[x].push_back(y);
+            l_Adj[y].push_back(x);
         }
     }
-    return gr;
+    return l_Adj;
+}
+void output(map<int, list<int>> gr) { //функция вывода
+    for (auto it = gr.begin(); it != gr.end(); it++) {
+        cout << "point " << it->first << ": ";
+        list<int> a = it->second;
+        for (auto iter = a.begin(); iter != a.end(); iter++)
+            cout << *iter << " ";
+        cout << endl;
+    }
 }
 
-int main(){
-    setlocale(LC_ALL, "ru");
+int main() {
+    cout << "A and B: ";
+    int a, b;
+    cin >> a >> b;
     map<int, list<int>> gr = MakeGr(0);
-    int p = 0;
-    cout << "Введите точку: ";
-    cin >> p;
-    if (gr[p].size() == 0)
-        cout << "Такой точки нет";
-    else{
-        cout << "Кол-во соседних точек: ";
-        cout << gr[p].size() << endl;
-    }
+    gr[a].remove(b); //просто удаляем путь из a в b 
+    gr[b].remove(a); //и путь из b в a 
+    cout << "Done: " << endl;
+    output(gr);
+    return 0;
 }
